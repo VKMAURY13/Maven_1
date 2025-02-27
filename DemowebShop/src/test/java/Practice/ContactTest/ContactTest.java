c924fd72e3268d5719a0fc46775dacfd53f7306a
air <air@airs-air.mshome.net>
2025-02-27 10:56:42 +0530

    commit 27022025_1056

---
+++ DemowebShop/src/test/java/Practice/ContactTest/ContactTest.java
@@ -0,0 +1,34 @@
+package Practice.ContactTest;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+	
+	
+	@Test
+	public void createContact()
+	{
+		System.out.println("Contact is Created");
+	}
+	
+
+	@Test
+	public void modifyContact()
+	{
+		System.out.println("Contact is modified");
+	}
+	
+	@Test
+	public void reviewContact()
+	{
+		System.out.println("Contact is reviewed");
+	}
+	
+	@Test
+	public void approveContact()
+	{
+		System.out.println("Contact is approved");
+	}
+
+
+}
---
+++ DemowebShop/src/test/java/Practice/Test/CreateOrgTest.java
@@ -0,0 +1,32 @@
+package Practice.Test;
+
+import org.testng.annotations.Test;
+
+public class CreateOrgTest {
+	
+	@Test
+	public void createOrg()
+	{
+		System.out.println("Organization is Created");
+	}
+	
+
+	@Test
+	public void modifyOrg()
+	{
+		System.out.println("Organization is modified");
+	}
+	
+	@Test
+	public void reviewOrg()
+	{
+		System.out.println("Organization is reviewed");
+	}
+	
+	@Test
+	public void approveOrg()
+	{
+		System.out.println("Organization is approved");
+	}
+
+}
